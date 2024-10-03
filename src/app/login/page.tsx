f4dbb46237c32571290ba2a8b068781c6f1f2563
Abhik Patra <theabhik2020@gmail.com>
2024-10-04 02:27:25 +0530

    set up login ui

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import Login from "@/components/Auth/Login";
+import React from "react";
+
+function page() {
+  return (
+    <div className="w-screen flex justify-center items-center h-screen overflow-hidden bg-slate-400">
+      <Login />
+    </div>
+  );
+}
+
+export default page;
